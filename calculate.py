5aa9bb82befcec5f04ab19ec6390607d78473490
Caitlin <caitlin.colbert@gmail.com>
2015-02-03 21:46:47 -0800

    updates

--- calculate.py
+++ calculate.py
@@ -5,21 +5,26 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 
 def calculate():
-    if request.method == "POST":
-        num1 = int(request.form['x'])
-        num2 = int(request.form['y'])
-        if request.form.get('0'):
-          result = (num1 + num2)
-        elif request.form.get('1'):
-          result = (num1 - num2)
-        elif request.form.get('2'):
-          result = (num1 * num2)
-        elif request.form.get('3'):
-          result = (num1 / num2)
-        return render_template("index.html", result=result)        
+  error = []
+  result = []
+  if request.method == "POST":
+    num1 = int(request.form['x'])
+    num2 = int(request.form['y'])
+    if request.form.get('add'):
+      result = (num1 + num2)
+    elif request.form.get('subtract'):
+      result = (num1 - num2)
+    elif request.form.get('multiply'):
+      result = (num1 * num2)
+    elif request.form.get('divide'):
+      if num2 == 0:
+        error = "Cannot divide by zero"
+      else:
+        result = (num1 / num2)
+    return render_template("index.html", result=result, error=error)        
 
-    else:
-        return render_template("index.html")
+  else:
+      return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/app.js
+++ static/js/app.js
@@ -1,38 +1,3 @@
 $(document).ready(function () {
-    // $("#calc").on("submit", function (event) {
-    //     event.preventDefault();
-    //     console.log("submit");
-        // calculate();
-    // });
-    // $("input").change(function(){
-    //     calculate();
-    // });
-    // $("select").change(function(){
-    //     calculate();
-    // });
-
-    // function calculate() {
-    //     var form = document.getElementById("calc");
-    //     var answer = form.elements["out"];
-    //     var x = parseInt(form.elements["x"].value);
-    //     var y = parseInt(form.elements["y"].value);
-    //     var operator = parseInt(form.elements["operator"].value);
-
-    //     switch(operator) {
-    //         case 0:
-    //         answer.value = x+y;
-    //         break;
-    //         case 1:
-    //         answer.value = x-y;
-    //         break;
-    //         case 2:
-    //         answer.value = x*y;
-    //         break;
-    //         case 3:
-    //         answer.value = (x/y).toFixed(2);
-    //         break;
-    //         default: 
-    //         break;
-    //     }
-    // }
+   
 });
