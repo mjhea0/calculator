997939ff120d78d8a96b29b6494f09d32277cd75
Caitlin <caitlin.colbert@gmail.com>
2015-02-08 13:13:23 -0800

    UI updates

--- static/js/app.js
+++ static/js/app.js
@@ -1,33 +1,27 @@
 $(document).ready(function () {
     event.preventDefault();
+
     // global variables
     var numbers = []
     var operator = []
 
-    $(".number").click(function(){ 
-        curValue = $("input").val();
-        if (curValue != 0) {
-            num1 = curValue;
-        }
-        console.log(numbers);
-        if (curValue <= 1) {
-            $("input[name='x']").val($(this).text());
+    function grabOperator() {
+        input = $("input[name='x']").val();
+        if (input == "") {
+            num1 = $('.output').text();
+            console.log(num1);
         }
         else {
-            $("input[name='x']").val(curValue + $(this).text());
+            num1 = $("input[name='x']").val();
+            console.log(num1);
         }
-    })
-
-    $(".operate").click(function(event){ 
-        num1 = $("input[name='x']").val();
         numbers.push(num1);
         console.log(numbers);
-        operator = $(this).prop('value');
         console.log(operator);
         $("input").val('');
-    });
-      
-    $(".equals").click(function(event){ 
+    }
+
+    function calculate() {
         num1 = numbers[0];
         num2 = $("input[name='x']").val();
         numbers.push(num2);
@@ -55,17 +49,66 @@ $(document).ready(function () {
                 console.log(error);
             }
         });
+    }
 
+    $(".number").click(function(){ 
+        curValue = $("input").val();
+        if (curValue != 0) {
+            num1 = curValue;
+        }
+        console.log(numbers);
+        if (curValue < 1) {
+            $("input[name='x']").val($(this).text());
+        }
+        else {
+            $("input[name='x']").val(curValue + $(this).text());
+        }
+    })
+
+    $(".operate").click(function(event){ 
+        operator = $(this).prop('value');
+        grabOperator();
+    });
+
+    $(document).keydown(function(e) {
+        if (e.keyCode == 107) {
+        console.log('addition pressed');
+        operator = 'add';
+        grabOperator();
+        }
+        if (e.keyCode == 109) {
+        console.log('subtraction pressed');
+        operator = 'subtract';
+        grabOperator();
+        }
+        if (e.keyCode == 106) {
+        console.log('multiplication pressed');
+        operator = 'multiply';
+        grabOperator();
+        }
+        if (e.keyCode == 111) {
+        console.log('division pressed');
+        operator = 'divide';
+        grabOperator();
+        }
+        if (e.keyCode == 13) {
+        console.log('equals pressed');
+        calculate();
+        }
     });
       
-    $(".clear").click(function(){ 
-        console.log("clear");
+    $(".equals").click(function(event){ 
+        calculate();
+    });
+    
+    function clear() {
+         console.log("clear");
         $("input").val('');
         $(".output").text('');
-    }); 
-      
-    $(".equals").click(function(){ 
-        console.log('equals');
+        numbers = []
+    }
+    $(".clear").click(function(){ 
+        clear();
     }); 
 
 });
